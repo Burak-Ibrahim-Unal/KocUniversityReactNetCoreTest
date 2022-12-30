05381711efa106c4462c0af1f3c5e9cd42f01d51
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-12-30 09:51:47 +0300

    student,role and user entities are readt // Persistence layer is readt to first use

---
+++ server/KocUniversityDemo/src/KUSYSDemo/Persistence/Contexts/BaseDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Contexts
+{
+    public class BaseDbContext : IdentityDbContext<User, Role, int>
+    {
+        public BaseDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Role>()
+            .HasData(
+                    new Role { Id = 1, Name = "Admin", NormalizedName = "ADMIN" },
+                    new Role { Id = 2, Name = "User", NormalizedName = "USER" }
+                );
+        }
+    }
+}
---
+++ server/KocUniversityDemo/src/KUSYSDemo/Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<BaseDbContext>(option =>
+                option.UseNpgsql(configuration.GetConnectionString("PostgresqlConnectionString")));
+
+            //services.AddScoped<IBrandRepository, BrandRepository>();
+            //services.AddScoped<IModelRepository, ModelRepository>();
+
+            return services;
+        }
+    }
+}
