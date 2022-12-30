bada9dced59b0ab3e40f03ac202f9cd250ecdbfb
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-12-30 08:49:00 +0300

    Unncessary codes are removed // new libraries

---
+++ server/KocUniversityDemo/src/CorePackages/Core.Persistence/Pagination/PaginationParams.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Persistence.Pagination
+{
+    public class PaginationParams
+    {
+        public const int MaxPageSize = 1000;
+        public int PageNumber { get; set; } = 1;
+        private int _pageSize = 50;
+        public int PageSize
+        {
+            get => _pageSize;
+            set => _pageSize = value > MaxPageSize ? MaxPageSize : value;
+        }
+    }
+}
---
+++ server/KocUniversityDemo/src/CorePackages/Core.Persistence/Repositories/ISyncRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Persistence.Repositories
+{
+    public interface ISyncRepository<T> where T : Entity
+    {
+        // linq,predicate,expression,func...
+        T Get(Expression<Func<T, bool>> predicate);
+
+        IPaginate<T> GetList(Expression<Func<T, bool>> predicate = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null,
+            int index = 0,
+            int size = 10,
+            bool enableTracking = true);
+
+
+        IQueryable<T> Query();
+        T Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
