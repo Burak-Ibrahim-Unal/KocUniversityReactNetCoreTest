e0bcd46374437459888441d01319e8f2385e7596
Burak Ibrahim Unal <burakibrahim@gmail.com>
2023-01-02 11:23:55 +0300

    course match backend updated

--- server/KocUniversityDemo/src/KUSYSDemo/Application/Features/CourseMatches/Dtos/CourseMatchListDto.cs
+++ server/KocUniversityDemo/src/KUSYSDemo/Application/Features/CourseMatches/Dtos/CourseMatchListDto.cs
@@ -9,6 +9,8 @@ namespace Application.Features.CourseMatches.Dtos
     public class CourseMatchListDto
     {
         public int Id { get; set; }
+        public int StudentTableId { get; set; }
+        public int CourseTableId { get; set; }
         public string StudentFirstName { get; set; }
         public string StudentLastName { get; set; }
         public string? StudentNumber { get; set; }
